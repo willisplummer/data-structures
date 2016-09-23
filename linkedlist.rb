45db679b389f156a8f21167c7774117c4c5510e0
Willis Plummer <willisplummer@gmail.com>
2016-09-23 01:19:51 -0400

    guestimates of O notation, iterate method, to_a, and some fixes for the tests

--- linkedlist.rb
+++ linkedlist.rb
@@ -8,11 +8,38 @@ end
 
 class LinkedList
   attr_accessor :head
+  #O(1)
   def initialize(value)
     @head = Node.new(nil, value)
   end
 
+  #O(n)
+  def iterate(accum=0, &block)
+    ref = head
+    while !ref.nil?
+      accum = block.call(accum, ref)
+      ref = ref.pointer
+    end
+    accum
+  end
+
+  #O(n)
+  def length
+    iterate do |count, node|
+      count += 1
+    end
+  end
+
+  #O(n)
+  def to_a
+    iterate([]) do |array, node|
+      array += [node.value]
+    end
+  end
+
+# O(n)
   def [](n)
+    raise ArgumentError.new "#{n} can't be negative" if n < 0
     element = head
     n.times do
       element = element.pointer
@@ -21,38 +48,48 @@ class LinkedList
     element
   end
 
+# O(n)
   def []=(n, value)
-    new_element = Node.new(self[n], value)
-    self[n-1].pointer = new_element
+    if n == 0
+      prepend(value)
+    elsif n > length || n < 0
+      raise ArgumentError.new "#{n} must be less than or equal to size of this linked list"
+    else
+      new_element = Node.new(self[n], value)
+      self[n-1].pointer = new_element
+    end
   end
 
+#O(1)
   def prepend(value)
     new_front = Node.new(head, value)
     self.head = new_front
+    self
   end
 
-  def length
-    count = 0
-    pointer = head
-    while !pointer.nil?
-      count += 1
-      pointer = pointer.pointer
-    end
-    count
-  end
-
+# O(n)
   def last
     self[length-1]
   end
 
+# O(n^2)
   def drop
-    self[length-2].pointer = nil
+    l = length
+    if l == 1
+      self.head = nil
+    else
+      self[l-2].pointer = nil
+    end
+    self
   end
 
+# O(n^2)
   def append(value)
     self[length]=value
+    self
   end
 
+# O(n^5)
   def reverse
     new_list = LinkedList.new(self.last.value)
     self.drop
